e163ed0e83558df1612c5a9c8a1302ca1e88e14d
Pierre <pierre.narcisi@epitech.eu>
2017-05-20 18:39:47 +0200

    term_function2

---
+++ src/termcaps_functions2.c
@@ -0,0 +1,19 @@
+/*
+** termcaps_functions2.c for 42sh in /home/byliarus/work/PSU_2016_42sh/src/
+**
+** Made by Pierre Narcisi
+** Login   <pierre.nacisi@epitech.eu>
+**
+** Started on  Fri May 19 19:08:51 2017 Pierre Narcisi
+** Last update Sat May 20 18:39:11 2017 Pierre Narcisi
+*/
+
+char *up_arrow(char *str, char c, int *cursor)
+{
+  return ();
+}
+
+char *down_arrow(char *str, char c, int *cursor)
+{
+  return ();
+}
